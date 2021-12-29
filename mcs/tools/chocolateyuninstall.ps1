a6da107706da0509f15e6312edca50c3c745a1e3
jreleaserbot <jreleaser@kordamp.org>
2021-12-29 16:24:43 +0000

    mcs v0.1.18

--- mcs/tools/chocolateyuninstall.ps1
+++ mcs/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
-# Generated with JReleaser 0.10.0 at 2021-12-29T13:16:17.546218209Z
+# Generated with JReleaser 0.10.0 at 2021-12-29T16:24:31.647081558Z
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'mcs-0.1.17-windows-x86_64'
+$app_home = Join-Path $package 'mcs-0.1.18-windows-x86_64'
 $app_exe = Join-Path $app_home 'bin/mcs.exe'
 
 Uninstall-BinFile -Name 'mcs' -Path $app_exe
