99f0f786a63bd42b19aaf4e261aca668917470fb
Maarten Mulders <m.th.mulders@xs4all.nl>
2022-06-20 21:30:41 +0200

    Manual update to 0.2.1

--- mcs/tools/chocolateyinstall.ps1
+++ mcs/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-# Generated with JReleaser 0.10.0 at 2022-02-20T12:19:48.835094445Z
+# Generated with JReleaser 0.10.0 at 2022-02-20T12:19:48.835094445Z, updated manually
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'mcs-0.2-windows-x86_64'
+$app_home = Join-Path $package 'mcs-0.2.1-windows-x86_64'
 $app_exe = Join-Path $app_home 'bin/mcs.exe'
 
 Install-ChocolateyZipPackage `
     -PackageName 'mcs' `
-    -Url 'https://github.com/mthmulders/mcs/releases/download/v0.2/mcs-0.2-windows-x86_64.zip' `
-    -Checksum '61b3e1a816714e8193571f2ce716bb6223898787bf1a42408181c9039c0ea636' `
+    -Url 'https://github.com/mthmulders/mcs/releases/download/v0.2.1/mcs-0.2.1-windows-x86_64.zip' `
+    -Checksum '59e0357cd254c3d954b7755c330abe4d40a31766eb931c0b5a0e4bd4aa297717' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
 
--- mcs/tools/chocolateyuninstall.ps1
+++ mcs/tools/chocolateyuninstall.ps1
@@ -1,7 +1,7 @@
-# Generated with JReleaser 0.10.0 at 2022-02-20T12:19:48.835094445Z
+# Generated with JReleaser 0.10.0 at 2022-02-20T12:19:48.835094445Z, updated manually
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'mcs-0.2-windows-x86_64'
+$app_home = Join-Path $package 'mcs-0.2.1-windows-x86_64'
 $app_exe = Join-Path $app_home 'bin/mcs.exe'
 
 Uninstall-BinFile -Name 'mcs' -Path $app_exe
