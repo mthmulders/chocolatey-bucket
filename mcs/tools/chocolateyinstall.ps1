645a1334496acc608f96d08e91fddd17f4f0589b
jreleaserbot <jreleaser@kordamp.org>
2023-01-04 18:48:45 +0100

    mcs v0.2.4

--- mcs/tools/chocolateyinstall.ps1
+++ mcs/tools/chocolateyinstall.ps1
@@ -1,14 +1,10 @@
-# Generated with JReleaser 0.10.0 at 2022-02-20T12:19:48.835094445Z, updated manually
+# Generated with JReleaser 1.2.0 at 2023-01-04T18:48:40.097981+01:00
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
-$app_home = Join-Path $package 'mcs-0.2.1-windows-x86_64'
-$app_exe = Join-Path $app_home 'bin/mcs.exe'
 
 Install-ChocolateyZipPackage `
     -PackageName 'mcs' `
-    -Url 'https://github.com/mthmulders/mcs/releases/download/v0.2.1/mcs-0.2.1-windows-x86_64.zip' `
-    -Checksum '59e0357cd254c3d954b7755c330abe4d40a31766eb931c0b5a0e4bd4aa297717' `
+    -Url 'https://github.com/mthmulders/mcs/releases/download/v0.2.4/mcs-0.2.4-windows-x86_64.zip' `
+    -Checksum 'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
-
-Install-BinFile -Name 'mcs' -Path $app_exe
