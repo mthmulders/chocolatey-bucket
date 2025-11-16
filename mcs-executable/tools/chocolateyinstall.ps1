929e0cf83cc2a66858e5c9f5370b9810ecef21c7
jreleaserbot <jreleaser@kordamp.org>
2025-11-16 14:39:23 +0100

    mcs-executable v0.9.2-rc-7

--- mcs-executable/tools/chocolateyinstall.ps1
+++ mcs-executable/tools/chocolateyinstall.ps1
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.20.0 at 2025-11-16T14:25:54.453327+01:00
+# Generated with JReleaser 1.20.0 at 2025-11-16T14:38:57.894967+01:00
 $tools = Split-Path $MyInvocation.MyCommand.Definition
 $package = Split-Path $tools
 
 Install-ChocolateyZipPackage `
     -PackageName 'mcs-executable' `
-    -Url 'https://github.com/mthmulders/mcs/releases/download/v0.9.2-rc-6/mcs-0.9.2-rc-6-windows-x86_64.zip' `
-    -Checksum '2111c56b86dcec0893da2155b6c01b0a7f79104a69e63df566e750e0185c1dc4' `
+    -Url 'https://github.com/mthmulders/mcs/releases/download/v0.9.2-rc-7/mcs-0.9.2-rc-7-windows-x86_64.zip' `
+    -Checksum 'dcfc5a583c4fe7196d16f3ad3c5dd24dbb1f994e99d90dc7b12644aa77f399fa' `
     -ChecksumType 'sha256' `
     -UnzipLocation $package
